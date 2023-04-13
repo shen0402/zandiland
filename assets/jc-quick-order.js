fa48c6c9cec1cd4e81ef4163a9e5515ea811188e
shen0402 <shenzaijun0402@gmail.com>
2023-04-13 14:24:12 -0700

    fix updating total price

--- assets/jc-quick-order.js
+++ assets/jc-quick-order.js
@@ -1,3 +1,11 @@
+function updatePrice($addToCartForm) {
+    var price = $addToCartForm.data('product-price');
+    var qty = $addToCartForm.find('.jc-js-quantity-selector').val();
+    var item_total_price = price * qty / 100;
+    item_total_price = item_total_price.toFixed(2);
+    $addToCartForm.find('.money-inner-total').text(item_total_price);
+}
+
 $(document).on('click', '.jc-quantity-btn-down', function(e){
     e.preventDefault();
     var input = $(this).closest('.jc-product-quantity-wrapper').find('.jc-js-quantity-selector');
@@ -5,6 +13,7 @@ $(document).on('click', '.jc-quantity-btn-down', function(e){
     if (qty >= 0) {
         input.val(qty);
     }
+    updatePrice($(this).closest('.quick-order-form'));
 });
 
 $(document).on('click', '.jc-quantity-btn-up', function(e){
@@ -12,4 +21,5 @@ $(document).on('click', '.jc-quantity-btn-up', function(e){
     var input = $(this).closest('.jc-product-quantity-wrapper').find('.jc-js-quantity-selector');
     var qty = Number(input.val()) + 6;
     input.val(qty);
+    updatePrice($(this).closest('.quick-order-form'));
 });
